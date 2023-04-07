96281a08df83c76bc7d14c8d3126a21a65f9c4e8
s-almas786 <samaralmas8@gmail.com>
2023-04-07 21:41:41 +0500

    mongoose

--- pro_academy_express/server.js
+++ pro_academy_express/server.js
@@ -1,6 +1,55 @@
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config({ path: "./config.env" });
+
 const app = require("./index");
 
-console.log(app.get("env"));
+mongoose
+  .connect(process.env.CONN_STR, {
+    useNewUrlParser: true,
+  })
+  .then((connec) => {
+    console.log("Connection successful");
+  })
+  .catch((err) => {
+    console.log("An error occured");
+  });
+
+const movieSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Title is required"],
+    unique: true,
+  },
+  year: Number,
+  runtime: {
+    type: Number,
+    required: [true, "Runtime is required"],
+  },
+  genre: [String],
+  director: String,
+  ratinfg: {
+    type: Number,
+    default: 1.0,
+  },
+});
+
+const Movie = mongoose.model("Movies", movieSchema);
+
+const testMovie = new Movie({
+  title: "Beetlejuice",
+  year: "1988",
+  runtime: "92",
+  genres: ["Comedy", "Fantasy"],
+  director: "Tim Burton",
+  rating: 5.0,
+});
+
+testMovie
+  .save()
+  .then((movie) => console.log(movie))
+  .catch((err) => console.log("Error saving movie " + err));
 
 const PORT = 3000;
 app.listen(PORT, () => {
